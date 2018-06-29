0ce36e1935ee599c759a6c4b4f7bd87553e3e8de
sk <semaria.eyzac@gmail.com>
2018-06-29 10:59:29 -0700

    update the App file by adding AppComponent wrapper to check the correct reloading of app and catch possible errors and add an if statment to trigger swapping process

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { AppContainer } from 'react-hot-loader';
 
-
+const render = (Component) => {
 ReactDOM.render(
-  <App/>,
+  <AppContainer>
+    <Component/>
+  </AppContainer>,
   document.getElementById('react-app-root')
 );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
