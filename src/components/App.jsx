91f2980919cd3baa18edc1be5ba50e48b46c515e
sk <semaria.eyzac@gmail.com>
2018-06-29 09:16:45 -0700

    make all my components and modify the index.jsx file accordingly

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Beer Haus</h1>
+      <h3>name: Ruby Zozzle</h3>
+      <h3>brewer: hi-wheel</h3>
+      <p>description: sparkling wine</p>
+      <p>abv: 6.8 %</p>
+      <p>price: 7</p>
+      <p>remaining: 20</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Beer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Beer(){
+  return ();
+}
+
+export default Beer;
