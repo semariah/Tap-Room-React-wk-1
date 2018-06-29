f9efdce4eb09bc3da36b5e1638366b611e227d6d
sk <semaria.eyzac@gmail.com>
2018-06-29 11:17:33 -0700

    add an npm run start scripts to automatically bundle and launch dev server

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'Tap Room',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
