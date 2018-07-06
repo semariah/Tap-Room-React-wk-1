362352909725c832abf32b135c878876caac803e
sk <semaria.eyzac@gmail.com>
2018-07-06 16:16:19 -0700

    create a beer detail file to select a specific beer

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import BeerList from './BeerList'
+import BeerDetail from './BeerDetail'
 
 function Admin(props){
   console.log(props.currentRouterPath)
   return (
     <div>
       <h2>Admin</h2>
+        <BeerDetail />
       <BeerList beerList={props.beerList}
         currentRouterPath={props.currentRouterPath} />
     </div>
---
+++ src/components/BeerDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BeerDetail(){
+  return (
+    <h2>Beer Details will be visible here soon!</h2>
+  );
+}
+
+export default BeerDetail;
