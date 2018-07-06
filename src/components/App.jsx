2e75e923147538a50a01c66dbefc9be6665e818a
sk <semaria.eyzac@gmail.com>
2018-07-06 11:10:25 -0700

    remove the masterBeerList hardcode to be lifted to App component and refactore App component into a class based

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,25 @@ import { Switch, Route } from 'react-router-dom'
 import Error404 from './Error404'
 import BeerControl from './BeerControl'
 
-function App(){
-  return (
-    <div>
-      <style jsx global>{`
-        body{
-          font-family: sans-serif;
-        }
-      `}</style>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={BeerList} />
-        <Route path='/newbeer' component={BeerControl} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  )
+class App extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <style jsx global>{`
+          body{
+            font-family: sans-serif;
+          }
+        `}</style>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={BeerList} />
+          <Route path='/newbeer' component={BeerControl} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
