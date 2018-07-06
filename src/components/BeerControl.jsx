b7019e2afcf9574098c55ec381684a7071155f2b
sk <semaria.eyzac@gmail.com>
2018-07-06 08:33:41 -0700

    create a new BeerControl, class-based stateful component and declare a state

---
+++ src/components/BeerControl.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class BeerControl extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+  
+  render(){
+    return (
+      <div>
+        <p>This is the BeerControl component!</p>
+      </div>
+    );
+  }
+}
+
+export default BeerControl;
