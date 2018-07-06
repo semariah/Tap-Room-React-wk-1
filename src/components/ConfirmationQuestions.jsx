4f0fbe9d1f427ad2a0699cdc3a9eef829ada625c
sk <semaria.eyzac@gmail.com>
2018-07-06 10:12:43 -0700

    child component exceutes the call back function that automatically invokes corresponding parent comonent via the props passed to child component to render the newBeerForm

--- src/components/ConfirmationQuestions.jsx
+++ src/components/ConfirmationQuestions.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function ConfirmationQuestions(){
+function ConfirmationQuestions(props){
   return(
     <div>
       <p> Do you want to add a new beer to the list? </p>
-        <button onClick={props.onTroubleshootingConfirmation}>Yes</button>
+      <button onClick={props.onTroubleshootingConfirmation}>Yes</button>
     </div>
   )
 }
