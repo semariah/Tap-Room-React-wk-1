80978c3b19ddfae16ea57d1aebc8c484e1ae4018
sk <semaria.eyzac@gmail.com>
2018-07-06 08:47:58 -0700

    troubleshooting to check the state created works by changig from false to true and works fine

--- src/components/BeerControl.jsx
+++ src/components/BeerControl.jsx
@@ -7,12 +7,20 @@ class BeerControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     }
+    this.handleClick = this.handleClick.bind(this)
   }
-  
+
+  handleClick(){
+    this.setState({formVisibleOnPage: true})
+    console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage)
+  }
+
   render(){
     return (
       <div>
         <p>This is the BeerControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+
       </div>
     )
   }
