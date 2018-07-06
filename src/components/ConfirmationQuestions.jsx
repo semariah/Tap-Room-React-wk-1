582e5efd2375f3a2fca260d1ce73ef8d6cc7f283
sk <semaria.eyzac@gmail.com>
2018-07-06 09:02:51 -0700

    create a new component called ConfirmationQuestions for adding more beers to the list

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ConfirmationQuestions(){
+  return(
+    <div>
+      <p> Do you want to add a new beer to the list? </p>
+        <button>Yes</button>
+    </div>
+  );
+}
